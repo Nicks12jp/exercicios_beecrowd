d9e828ffa90b2104081644a1059ec71a0e3e5999
Nicolas <nicks12jp@gmail.com>
2025-03-17 11:27:01 -0300

    Update exercicio_1010.c

--- exercicio_1010.c
+++ exercicio_1010.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-int main() {
- int cod1, cod2, qtd1, qtd2; 
- double valor1, valor2, total;
- 
-//Entrada de dados:
- scanf("%d %d %lf", &cod1, &qtd1, &valor1);
- 
- scanf("%d %d %lf", &cod2, &qtd2, &valor2);
 
-//Calculo total: 
- total =(qtd1*valor1)+(qtd2*valor2);
-
-//Saida de dados: 
- printf("VALOR A PAGAR : R$ %.2lf\n",total);
-
-    return 0;
-}
