15005342d661270063c00304169bd3538bf78114
Nicolas <nicks12jp@gmail.com>
2025-05-08 13:16:20 -0300

    Create exercicio_1016.java

---
+++ exercicio_1016.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int Y = sc.nextInt();
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+    int Distancia = (Y * 2);
+    
+    System.out.println(Distancia + " minutos");
+    sc.close();
+    }
+ 
+}
